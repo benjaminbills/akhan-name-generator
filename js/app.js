c2477ff25543a0c8f0cdca006f1cf879548d9cee
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-02-19 12:44:53 +0300

    get data using query selector

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+const userForm = document.querySelector("#userForm");
+const resultContainer = document.querySelector("#result");
+const gendersValue = () => {
+  if (document.getElementById("male").checked) {
+    return document.getElementById("male").value;
+  }
+  if (document.getElementById("female").checked) {
+    return document.getElementById("female").value;
+  }
+  return false;
+};
+console.log(userForm);
+console.log(gendersValue);
